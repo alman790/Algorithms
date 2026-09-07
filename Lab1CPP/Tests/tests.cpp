e22b00b05f6bd2c16ea35d6dbcefa3f667ad1058
vladlevin790 <vladlevin790@gmail.com>
2026-09-07 20:46:45 +0300

    feat: added more test task only

--- Lab1CPP/Tests/tests.cpp
+++ Lab1CPP/Tests/tests.cpp
@@ -35,9 +35,9 @@ bool run_program(std::string &program, int task) {
 
 void check(std::string &name, std::string &actual, std::string &expected) {
     if (actual == expected) {
-        std::cout << name << ": PASSED" << "\n";
+        std::cout << "[PASSED] : " << name  << "\n";
     } else {
-        std::cout << name << ": FAILED" << "\n";
+        std::cout << "[FAILED] : " << name << "\n";
         std::cout << actual << " != " << expected << "\n";
         FAILED_TESTS++;
     }
@@ -62,10 +62,262 @@ void test_task1(std::string &program) {
         }
     }
 
-    // {
-    //     clear_out();
-    //
-    // }
+    {
+        clear_out();
+        std::string test = "1\n"
+                           "1";
+        write_inp(test);
+
+        if (!run_program(program, 1)) {
+            std::cout <<  "[FAILED] task1: programm error " << "\n";
+            FAILED_TESTS++;
+        } else {
+            std::string exp = "1\n";
+            std::string actual = read_out();
+            std::string name = "task1: one int";
+            check(name, actual, exp);
+        }
+    }
+
+    {
+        clear_out();
+        std::string test = "1\n"
+                           "-1\n";
+        write_inp(test);
+        if (!run_program(program, 1)) {
+            std::cout <<  "[FAILED] task1: programm error " << "\n";
+            FAILED_TESTS++;
+        } else {
+            std::string exp = "-1\n";
+            std::string actual = read_out();
+            std::string name = "task1: one negative int";
+            check(name, actual, exp);
+        }
+    }
+
+    {
+        clear_out();
+        std::string test = "2\n"
+                           "21 5";
+        write_inp(test);
+        if (!run_program(program, 1)) {
+            std::cout <<  "[FAILED] task1: programm error " << "\n";
+            FAILED_TESTS++;
+        } else {
+            std::string exp = "16\n";
+            std::string actual = read_out();
+            std::string name = "task1: two els";
+            check(name, actual, exp);
+        }
+    }
+
+    {
+        clear_out();
+        std::string test = "4\n"
+                           "4 -1 3 -3";
+        write_inp(test);
+        if (!run_program(program, 1)) {
+            std::cout <<  "[FAILED] task1: programm error " << "\n";
+            FAILED_TESTS++;
+        } else {
+            std::string exp = "11\n";
+            std::string actual = read_out();
+            std::string name = "task1: negative els in subtract position";
+            check(name, actual, exp);
+        }
+    }
+
+    {
+        clear_out();
+        std::string test = "6\n"
+                           "-1 -2 -3 -4 -6 -5";
+        write_inp(test);
+        if (!run_program(program, 1)) {
+            std::cout <<  "[FAILED] task1: programm error " << "\n";
+            FAILED_TESTS++;
+        } else {
+            std::string exp = "1\n";
+            std::string actual = read_out();
+            std::string name = "task1: only negatives";
+            check(name, actual, exp);
+        }
+    }
+
+    {
+        clear_out();
+        std::string test = "7\n"
+                           "-1 1 -2 2 -3 3 -5";
+        write_inp(test);
+        if (!run_program(program, 1)) {
+            std::cout <<  "[FAILED] task1: programm error " << "\n";
+            FAILED_TESTS++;
+        } else {
+            std::string exp = "-17\n";
+            std::string actual = read_out();
+            std::string name = "task1: mix";
+            check(name, actual, exp);
+        }
+    }
+
+    {
+        clear_out();
+        std::string test = "10\n"
+                           "10 10 10 10 10 10 10 10 10 10";
+        write_inp(test);
+        if (!run_program(program, 1)) {
+            std::cout <<  "[FAILED] task1: programm error " << "\n";
+            FAILED_TESTS++;
+        } else {
+            std::string exp = "0\n";
+            std::string actual = read_out();
+            std::string name = "task1: only ten";
+            check(name, actual, exp);
+        }
+    }
+
+    {
+        clear_out();
+        std::string test = "0\n";
+        write_inp(test);
+        if (!run_program(program, 1)) {
+            std::cout <<  "[FAILED] task1: programm error " << "\n";
+            FAILED_TESTS++;
+        } else {
+            std::string exp = "0\n";
+            std::string actual = read_out();
+            std::string name = "task1: zero";
+            check(name, actual, exp);
+        }
+    }
+}
+
+void test_task2(std::string &program) {
+    {
+        clear_out();
+        std::string test = "8\n"
+                           "100 7 42 10 11 40 2 70";
+        write_inp(test);
+        if (!run_program(program, 2)) {
+            std::cout <<  "[FAILED] task2: programm error " << "\n";
+            FAILED_TESTS++;
+        } else {
+            std::string exp = "40 42\n";
+            std::string actual = read_out();
+            std::string name = "task2: shuffled arr";
+            check(name, actual, exp);
+        }
+    }
+
+    {
+        clear_out();
+        std::string test = "7\n"
+                           "18 5 100 18 3 40 2";
+        write_inp(test);
+        if (!run_program(program, 2)) {
+            std::cout <<  "[FAILED] task2: programm error " << "\n";
+            FAILED_TESTS++;
+        } else {
+            std::string exp = "18 18\n";
+            std::string actual = read_out();
+            std::string name = "task2: duplicate";
+            check(name, actual, exp);
+        }
+    }
+
+    {
+        clear_out();
+        std::string test = "8\n"
+                           "-101 -100 -3 8 -6 15 -8 50";
+        write_inp(test);
+        if (!run_program(program, 2)) {
+            std::cout <<  "[FAILED] task2: programm error " << "\n";
+            FAILED_TESTS++;
+        } else {
+            std::string exp = "-8 -6\n";
+            std::string actual = read_out();
+            std::string name = "task2: neg and pos";
+            check(name, actual, exp);
+        }
+    }
+
+    {
+        clear_out();
+        std::string test = "8\n"
+                           "20 101 102 103 7 40 70 200";
+        write_inp(test);
+        if (!run_program(program, 2)) {
+            std::cout <<  "[FAILED] task2: programm error " << "\n";
+            FAILED_TESTS++;
+        } else {
+            std::string exp = "20 40\n";
+            std::string actual = read_out();
+            std::string name = "task2: odd must be ign";
+            check(name, actual, exp);
+        }
+    }
+
+    {
+        clear_out();
+        std::string test = "8\n"
+                           "-200 150 -2 99 80 2 -50 300";
+        write_inp(test);
+        if (!run_program(program, 2)) {
+            std::cout <<  "[FAILED] task1: programm error " << "\n";
+            FAILED_TESTS++;
+        } else {
+            std::string exp = "-2 2\n";
+            std::string actual = read_out();
+            std::string name = "task2: around zero";
+            check(name, actual, exp);
+        }
+    }
+
+    {
+        clear_out();
+        std::string test = "1\n"
+                           "1";
+        write_inp(test);
+        if (!run_program(program, 2)) {
+            std::cout <<  "[FAILED] task1: programm error " << "\n";
+            FAILED_TESTS++;
+        } else {
+            std::string exp = "0 0\n";
+            std::string actual = read_out();
+            std::string name = "task2: one el";
+            check(name, actual, exp);
+        }
+    }
+
+    {
+        clear_out();
+        std::string test = "1\n"
+                           "2";
+        write_inp(test);
+        if (!run_program(program, 2)) {
+            std::cout <<  "[FAILED] task2: programm error " << "\n";
+            FAILED_TESTS++;
+        } else {
+            std::string exp = "2 0\n";
+            std::string actual = read_out();
+            std::string name = "task2: one even";
+            check(name, actual, exp);
+        }
+    }
+
+    {
+        clear_out();
+        std::string test = "0\n";
+        write_inp(test);
+        if (!run_program(program, 2)) {
+            std::cout <<  "[FAILED] task2: programm error " << "\n";
+            FAILED_TESTS++;
+        } else {
+            std::string exp = "0 0\n";
+            std::string actual = read_out();
+            std::string name = "task2: zero els";
+            check(name, actual, exp);
+        }
+    }
 }
 
 int main(int argc, char *argv[]) {
@@ -77,6 +329,7 @@ int main(int argc, char *argv[]) {
     std::string program = argv[1];
 
     test_task1(program);
+    test_task2(program);
 
     std::cout << '\n';
 
@@ -89,9 +342,3 @@ int main(int argc, char *argv[]) {
 
     return 1;
 }
-
-/*
- *
- * TODO: Add more test, improve the main lab1.cpp file, cause it's seems like shit, then dream about exceptions
- *
-*/
--- Lab1CPP/lab1.cpp
+++ Lab1CPP/lab1.cpp
@@ -41,31 +41,49 @@ void task2(Array *arr) {
         if (array_get(arr, i) % 2 == 0) count_even++;
     }
 
-    Array *even = array_create(count_even);
-    size_t j = 0;
-    for (size_t i = 0; i < array_size(arr); i++) {
-        if (array_get(arr, i) % 2 == 0) {
-            array_set(even, j, array_get(arr, i));
-            j++;
+    if (count_even == 0) {
+        array_set(res, 0, 0);
+        array_set(res, 1, 0);
+        writer(res);
+        array_delete(res);
+        array_delete(arr);
+    } else if (count_even == 1) {
+        size_t index = 0;
+        for (size_t i = 0; i < array_size(arr); i++) {
+            if (array_get(arr, i) % 2 == 0) index = i;
         }
-    }
-
-    if (count_even > 0) {
-        quicksort(even, 0, array_size(even) - 1);
-    }
-
-    for (size_t i = 0; i < array_size(even) - 1; i++) {
-        int diff = array_get(even, i + 1) - array_get(even, i);
-        if (diff < min) {
-            min = diff;
-            array_set(res, 0, array_get(even, i));
-            array_set(res, 1, array_get(even, i + 1));
+        array_set(res, 0, array_get(arr, index));
+        array_set(res, 1, 0);
+        writer(res);
+        array_delete(res);
+        array_delete(arr);
+    } else {
+        Array *even = array_create(count_even);
+        size_t j = 0;
+        for (size_t i = 0; i < array_size(arr); i++) {
+            if (array_get(arr, i) % 2 == 0) {
+                array_set(even, j, array_get(arr, i));
+                j++;
+            }
         }
+
+        if (count_even > 0) {
+            quicksort(even, 0, array_size(even) - 1);
+        }
+
+        for (size_t i = 0; i < array_size(even) - 1; i++) {
+            int diff = array_get(even, i + 1) - array_get(even, i);
+            if (diff < min) {
+                min = diff;
+                array_set(res, 0, array_get(even, i));
+                array_set(res, 1, array_get(even, i + 1));
+            }
+        }
+        writer(res);
+        array_delete(arr);
+        array_delete(even);
+        array_delete(res);
     }
-    writer(res);
-    array_delete(arr);
-    array_delete(even);
-    array_delete(res);
 }
 
 void quicksort(Array *arr, size_t left, size_t right) {
